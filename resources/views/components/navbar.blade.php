67e6e67b67c93595a296a887e17f95cf6d7b376a
Nabil Hamada <31300818+nabil12ful@users.noreply.github.com>
2022-08-29 20:24:11 +0200

    fix navbar in dashboard

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,14 @@
+@props(['active', 'href'])
+
+@php
+// $classes = ($active ?? false)
+//             ? 'active'
+//             : 'active';
+$classes = $active == $href ? 'active' : ''; 
+@endphp
+<li {{ $attributes->merge(['class' => $classes]) }}>
+    <a href="{{$href}}"">
+        {{ $slot }}
+    </a>
+</li>
+
--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -5,24 +5,24 @@
     <a href="#menu" class="menu-link"><i class="fa fa-bars"></i></a>
     <nav id="menu" class="main-nav" role="navigation">
         <ul class="main-menu">
-            <x-nav-link :href="route('home')" :active="request()->routeIs('home')">
+            <x-navbar :href="route('home')" :active="request()->routeIs('home')">
                 Home
-            </x-nav-link>
-            <x-nav-link :href="'sections'" :active="request()->routeIs('dashboard')">
+            </x-navbar>
+            <x-navbar :href="'sections'" :active="request()->routeIs('dashboard')">
                 Sections
-            </x-nav-link>
-            <x-nav-link :href="route('home')" :active="request()->routeIs('dashboard')">
+            </x-navbar>
+            <x-navbar :href="route('home')" :active="request()->routeIs('dashboard')">
                 Courses
-            </x-nav-link>
-            <x-nav-link :href="route('home')" :active="request()->routeIs('dashboard')">
+            </x-navbar>
+            <x-navbar :href="route('home')" :active="request()->routeIs('dashboard')">
                 Workshops
-            </x-nav-link>
-            <x-nav-link :href="route('home')" :active="request()->routeIs('dashboard')">
+            </x-navbar>
+            <x-navbar :href="route('home')" :active="request()->routeIs('dashboard')">
                 About Us
-            </x-nav-link>
-            <x-nav-link :href="route('home')" :active="request()->routeIs('dashboard')">
+            </x-navbar>
+            <x-navbar :href="route('home')" :active="request()->routeIs('dashboard')">
                 Countact US
-            </x-nav-link>
+            </x-navbar>
         </ul>
     </nav>
 </header>
