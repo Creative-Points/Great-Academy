ffd9bd1ed4dedeb11ccc2d3f52988728a1fd6631
Nabil Hamada <31300818+nabil12ful@users.noreply.github.com>
2022-09-17 23:32:00 +0200

    Optimize code & add frontend controller

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\CourseController;
+use App\Http\Controllers\FrontendController;
 use App\Http\Controllers\SectionController;
 use App\Http\Controllers\StudentController;
 use App\Http\Controllers\TestController;
@@ -19,11 +20,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('index');
-})->name('home');
+Route::controller(FrontendController::class)->group(function() {
+    // home page
+    Route::get('/', 'home')->name('home');
+    // about us page
+    Route::get('about', 'about')->name('about');
+    // contact us page
+    Route::get('contact-us', 'contact')->name('contact-us');
+});
 
-Route::get('test', [TestController::class, 'index']);
+// Route::get('test', [TestController::class, 'index']);
 // sections
 Route::controller(SectionController::class)->group(function() {
     Route::get('sections', 'index')->name('sections');
@@ -34,20 +40,7 @@ Route::controller(CourseController::class)->group(function() {
     Route::get('/courses', 'index')->name('courses');
     Route::get('course/{course:slug}', 'course')->name('course');
 });
-// about
-Route::get('/about', function () {
-    return view('about');
-})->name('about');
-// contact-us
-Route::get('/contact-us', function () {
-    return view('contact-us');
-})->name('contact-us');
-// login-studetn
-Route::get('/login-studetn', function () {
-    return view('login-studetn');
-})->name('login-studetn');
 // workshops
-
 Route::controller(WorkshopController::class)->group(function() {
     Route::get('/workshops', 'index')->name('workshops');
     Route::get('workshop/{workshop:slug}', 'workshop')->name('workshop');
